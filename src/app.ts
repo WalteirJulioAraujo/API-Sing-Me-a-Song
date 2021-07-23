a395a2eb4d07dfc9a84c166b2c5956a377e0d923
Walteir Araujo <walteiraraujo@poli.ufrj.br>
2021-07-23 03:00:51 -0300

    Testando a conexao com o banco

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
 import express from "express";
 import cors from "cors";
 
+import connection from "./database";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get("/test", (req, res) => {
   res.send("OK!");
+});
+
+app.post("/recommendations", async (req,res) => {
+  try{
+    const { name } = req.body;
+    const { youtubeLink } = req.body;
+    await connection.query(`INSERT INTO songs (name,"youtubeLink") VALUES ($1,$2)`,[name,youtubeLink]);
+    res.sendStatus(201);
+  }catch(error){
+    console.log(error);
+    res.sendStatus(500);
+  }
+
 });
 
 export default app;
--- src/database.ts
+++ src/database.ts
@@ -5,9 +5,11 @@ const { Pool } = pg;
 const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PASSWORD),
+  port: parseInt(process.env.DB_PORT),
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
 };
 
 const connection = new Pool(config);
+
+export default connection;

34b95e9bb8306a031b37d5c25a45077924d1ac9c
Walteir Araujo <walteiraraujo@poli.ufrj.br>
2021-07-23 04:50:28 -0300

    Começo dos testes

---
+++ tests/factories/songFactory.ts
@@ -0,0 +1,7 @@
+
+
+export async function createSong(){
+    const name = "teste";
+    const youtubeLink = "https://www.youtube.com/watch?v=GOFiGClu0uk&ab_channel=COLORS";
+    return {name,youtubeLink};
+}
--- tests/integration/sendSong.test.ts
+++ tests/integration/sendSong.test.ts
@@ -1,9 +1,13 @@
 import supertest from "supertest";
 import app from "../../src/app";
+import { createSong } from "../factories/songFactory"
 
 const agent = supertest(app);
 
 describe("GET /test", () => {
   it("returns status 201 for valid params", async () => {
+    const song = await createSong();
+    const response = await agent.post("/recommendations").send({ name:song.name, youtubeLink:song.youtubeLink });
+    expect(response.status).toBe(201);
   });
 });

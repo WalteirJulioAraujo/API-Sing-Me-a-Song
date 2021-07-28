3c666b15d3f905f3fda7728cfa2b99fb5a0f7fd7
Guga <contato.gustavobarbosa@outlook.com>
2021-07-28 18:32:43 -0300

    Preparando revisão do código do Walteir

--- tests/factories/songFactory.ts
+++
@@ -1,8 +0,0 @@
-import faker from "faker";
-
-export async function createSong(){
-    const name = faker.name.findName();
-    const youtubeLink = `https://www.youtube.com/watch?v=${faker.random.alphaNumeric(11)}`;
-    return {name,youtubeLink};
-}
-
